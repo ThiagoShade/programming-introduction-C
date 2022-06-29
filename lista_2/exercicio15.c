1046ef11f9c896df3823f5cf204c35a516fbaca7
Thiago "Shade" de Souza Filgueiras <thiagosf.987@gmail.com>
2022-06-29 14:06:21 -0300

    Add files via upload

---
+++ lista_2/exercicio15.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+
+    int i1, i2, i3, quantidade, quantidade_feita, numero1, numero2, soma_div1, soma_div2, div;
+    scanf("%d", &quantidade);
+
+    quantidade_feita = 0;
+    numero1 = 1;
+    while (quantidade_feita < quantidade) {
+        // scanf("%d", &numero1);
+
+        div = 1;
+        soma_div1 = 0;
+        for (i1 = numero1; i1 > 0; i1--) {
+            if (numero1 % div == 0) soma_div1 += div;
+            div++;
+        }
+
+        // printf("%d\n", soma_div1);
+
+        div = 1;
+        numero2 = numero1 - 1;
+        soma_div2 = 0;
+        for (i2 = numero1 - 1; i2 > 0; i2--) {
+            
+            for (i3 = numero2; i3 > 0; i3--) {
+                if (numero2 % div == 0) soma_div2 += div;
+                div++;
+            }
+
+            if (soma_div1 == numero2 && soma_div2 == numero1) {
+                printf("(%d,%d)\n", numero2, numero1);
+                quantidade_feita++;
+            }
+
+            // printf("%d\n%d\n", numero1, numero2);
+            // printf("%d\n%d\n", soma_div1, soma_div2);
+            numero2--;
+        }
+        // break;
+        numero1++;
+    }
+
+
+
+
+    return 0;
+}
---
+++ lista_2/exercicio18.c
@@ -0,0 +1,52 @@
+    #include <stdio.h>
+     
+    int main() {
+     
+        int N, num, primo, div,contador, contador2, verificador_primo, verificador_divisao;
+     
+        primo = 2;
+        contador = 0;
+     
+        
+        while (1) {
+            scanf("%d", &N);
+            if (N < 2) printf("Fatoracao nao e possivel para o numero %d!\n", N);
+            else break;
+        } 
+     
+        num = N;
+        while (num != 1) {
+     
+            div = primo;
+            verificador_primo = 1;
+            while (div > 1) {
+                if (primo % div == 0) {
+                    if (div != primo) verificador_primo = 0;
+                }
+                if (verificador_primo == 1) break;
+                div--;
+            }
+     
+     
+            if (num % primo == 0) {
+                verificador_divisao = 1;
+            }
+            contador2 = 0;
+            while (verificador_divisao == 1) {
+                if (contador != 0 && num != 1 && contador2 != 0) printf(" x ");
+                else if (contador == 0 && num != 1) printf("%d = ", N);
+                if (num % primo == 0) {
+                    printf("%d", primo);
+                    num /= primo;
+                } else verificador_divisao = 0;
+                contador++;
+                contador2++;
+            }
+            
+            primo++;
+        }
+     
+     
+     
+        return 0;
+    }
