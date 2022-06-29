cdda1388a57e4ce281fc8f046429d63ed37f8ea9
Thiago "Shade" de Souza Filgueiras <thiagosf.987@gmail.com>
2022-06-29 15:06:32 -0300

    Update exercicio15.c

--- lista_2/exercicio15.c
+++ lista_2/exercicio15.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int i1, i2, i3, quantidade, quantidade_feita, numero1, numero2, soma_div1, soma_div2, div;
+    int i1, i2, i3, quantidade, medidor, quantidade_feita, numero1, numero2, soma_div1, soma_div2, div;
     scanf("%d", &quantidade);
 
     quantidade_feita = 0;
@@ -12,29 +12,32 @@ int main() {
 
         div = 1;
         soma_div1 = 0;
-        for (i1 = numero1; i1 > 0; i1--) {
+        for (i1 = (numero1 / 2) + 1; i1 > 0; i1--) {
             if (numero1 % div == 0) soma_div1 += div;
             div++;
         }
 
         // printf("%d\n", soma_div1);
 
-        div = 1;
-        numero2 = numero1 - 1;
-        soma_div2 = 0;
-        for (i2 = numero1 - 1; i2 > 0; i2--) {
+        numero2 = numero1;
+        for (i2 = numero1/2; i2 > 0; i2--) {
             
-            for (i3 = numero2; i3 > 0; i3--) {
+            div = 1;
+            soma_div2 = 0;
+            for (i3 = (numero2 / 2) + 1; i3 > 0; i3--) {
                 if (numero2 % div == 0) soma_div2 += div;
+
                 div++;
             }
 
             if (soma_div1 == numero2 && soma_div2 == numero1) {
-                printf("(%d,%d)\n", numero2, numero1);
-                quantidade_feita++;
+                if (numero1 != numero2) {
+                    printf("(%d,%d)\n", numero2, numero1);
+                    quantidade_feita++;
+                }
             }
 
-            // printf("%d\n%d\n", numero1, numero2);
+            // printf("%d %d %d %d\n", numero1, numero2, soma_div1, soma_div2);
             // printf("%d\n%d\n", soma_div1, soma_div2);
             numero2--;
         }
