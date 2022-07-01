ee64520e355fe26255efdbc6755fcd4e11a63004
Thiago "Shade" de Souza Filgueiras <thiagosf.987@gmail.com>
2022-07-01 15:21:04 -0300

    Add files via upload

---
+++ class_notes/1049.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+void swap(int x, int y);
+void swapp(int* pa, int* pb);
+
+void swap (int x, int y) {
+   
+    int t;
+/marketplace
+    t = x;
+    x = y;
+    y = t;
+
+}
+
+void swapp (int* pa, int* pb) {
+
+    int t;
+
+    t = *pa;
+    *pa = *pb;
+    *pb = t;
+
+}
+
+int main() {
+
+    int a, b;
+
+    a = 10;
+    b = 20;
+    
+    printf("%d %d\n", a, b);
+
+    swap(a,b);
+
+    printf("%d %d\n", a, b);
+
+    swapp(&a, &b);
+
+    printf("%d %d\n", a, b);
+
+
+    return 0;
+}
