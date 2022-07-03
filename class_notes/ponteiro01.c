b108b5a3d8a7e30c958b6ab0bd31335992effab6
Thiago "Shade" de Souza Filgueiras <thiagosf.987@gmail.com>
2022-07-03 20:22:49 -0300

    edit ponteiro01.c

--- class_notes/ponteiro01.c
+++ class_notes/ponteiro01.c
@@ -2,7 +2,7 @@
 void swap(int x, int y);
 void swapp(int* pa, int* pb);
 
-void swap (int x, int y) {
+void swap (int x, int y) { // Essa função não altera os valores de a e b, pois o x e y são mudados mas os valores de a e b continuam os mesmos.
    
     int t;
     t = x;
@@ -11,7 +11,7 @@ void swap (int x, int y) {
 
 }
 
-void swapp (int* pa, int* pb) {
+void swapp (int* pa, int* pb) { // Já essa altera sim os valores, pois o escopo da função consegue alterar os valores na memória de a e b por meio dos ponteiros.
 
     int t;
 
@@ -20,6 +20,12 @@ void swapp (int* pa, int* pb) {
     *pb = t;
 
 }
+
+/* 
+Resumindo, uma das vantagens de trabalhar com ponteiros é poder trabalhar em diferentes
+escopos de funções conseguindo acessar valores que estão em outros escopos com muito mais facilidade. 
+*/
+
 
 int main() {
 
