906c68c34882c8f6121151eee1f58a6d4d1c9d8a
Thiago "Shade" de Souza Filgueiras <thiagosf.987@gmail.com>
2022-07-09 14:17:09 -0300

    Add notes

---
+++ class_notes/ponteiro03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+int main()  
+{  
+    int a=1;  
+    int b=2;  
+    int *const ptr = &a; // Como ptr é um ponteiro constante, a variável para o qual ele apontará deve ser inicializado na declaração, assim como qualquer outra constante. 
+    // ptr=&a;  Isso não pode ser feito pois ptr é um ponteiro constante e não pode ter seu endereço alterado.
+    // ptr=&b;  
+    printf("Value of ptr is :%d",*ptr);  
+    return 0;  
+}
